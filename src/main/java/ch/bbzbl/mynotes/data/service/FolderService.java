9d514c6bd707bbf6df6e7c9d62900b79a4c6616c
Burn Levi <100278990+e031000@users.noreply.github.com>
2023-03-17 10:14:49 +0100

    Services Update

--- src/main/java/ch/bbzbl/mynotes/data/service/FolderService.java
+++ src/main/java/ch/bbzbl/mynotes/data/service/FolderService.java
@@ -1,6 +1,41 @@
 package ch.bbzbl.mynotes.data.service;
 
+import ch.bbzbl.mynotes.data.entity.Folder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Optional;
+
 public class FolderService {
 
-    private FolderRepository folderRepository;
+    private FolderRepository repository;
+
+    public FolderService(FolderRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<Folder> get(Long id) {
+        return repository.findById(id);
+    }
+
+    public Folder update(Folder entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Page<Folder> list(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public Page<Folder> list(Pageable pageable, Specification<Folder> filter) {
+        return repository.findAll(filter, pageable);
+    }
+    public int count() {
+        return (int) repository.count();
+    }
+
 }
--- src/main/java/ch/bbzbl/mynotes/data/service/NoteService.java
+++ src/main/java/ch/bbzbl/mynotes/data/service/NoteService.java
@@ -1,6 +1,39 @@
 package ch.bbzbl.mynotes.data.service;
 
+import ch.bbzbl.mynotes.data.entity.Note;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Optional;
+
 public class NoteService {
 
-    private NoteRepository noteRepository;
+    private NoteRepository repository;
+    public NoteService(NoteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<Note> get(Long id) {
+        return repository.findById(id);
+    }
+
+    public Note update(Note entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Page<Note> list(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public Page<Note> list(Pageable pageable, Specification<Note> filter) {
+        return repository.findAll(filter, pageable);
+    }
+    public int count() {
+        return (int) repository.count();
+    }
 }
