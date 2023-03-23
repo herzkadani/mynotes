0201e404862807c6c3a12203a0540e3516f6ade1
iventhepro <iven.kuder@bbzbl-it.ch>
2023-03-23 11:31:17 +0100

    user login

--- src/main/java/ch/bbzbl/mynotes/views/mynotes/MyNotesView.java
+++ src/main/java/ch/bbzbl/mynotes/views/mynotes/MyNotesView.java
@@ -2,13 +2,18 @@ package ch.bbzbl.mynotes.views.mynotes;
 
 import ch.bbzbl.mynotes.data.entity.Folder;
 import ch.bbzbl.mynotes.data.entity.Note;
+import ch.bbzbl.mynotes.data.entity.User;
 import ch.bbzbl.mynotes.data.service.FolderService;
 import ch.bbzbl.mynotes.data.service.NoteService;
+import ch.bbzbl.mynotes.security.AuthenticatedUser;
 import ch.bbzbl.mynotes.views.MainLayout;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
@@ -16,6 +21,7 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.richtexteditor.RichTextEditor;
@@ -28,27 +34,32 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import jakarta.annotation.security.PermitAll;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Objects;
 
 @PageTitle("MyNotes")
-@RouteAlias(value = "", layout = MainLayout.class)
 @Route(value = "MyNotes", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
 @PermitAll
 public class MyNotesView extends Div {
 
 	public static final String LEER = "Leer";
 	public static final String MIN_WIDTH = "300px";
 	public static final String BEARBEITEN = "Bearbeiten";
-	private FolderService folderService = null;
-	private NoteService noteService = null;
 	private final VerticalLayout noteLayout = new VerticalLayout();
 	VirtualList<Folder> folderList = new VirtualList<>();
 	VirtualList<Note> noteList = new VirtualList<>();
 	RichTextEditor textEditor = new RichTextEditor();
 	SplitLayout splitLayout;
 	HorizontalLayout notesSplit = new HorizontalLayout();
+	private TextField popupField = new TextField();
+	@Autowired
+	private AuthenticatedUser authenticatedUser;
+	private User user;
+	private FolderService folderService = null;
+	private NoteService noteService = null;
 	private List<Folder> folders;
 	private List<Note> notes;
 	private Folder currentFolder;
@@ -142,13 +153,10 @@ public class MyNotesView extends Div {
 
 	private SplitLayout createContent() {
 
+		checkUser();
+
 		if (this.folderService.list().isEmpty()) {
-			currentFolder = new Folder(LEER, false, null, null);
-			folderService.update(currentFolder);
-			currentNote = new Note(LEER, LEER, currentFolder);
-			noteService.update(currentNote);
-			folders = folderService.list();
-			notes = folders.stream().findFirst().get().getNotes();
+			emptyFolder();
 		} else if (this.folderService.list().stream().findFirst().get().getNotes().isEmpty()) {
 			currentFolder = folders.stream().findFirst().orElse(null);
 			currentNote = new Note(LEER, LEER, currentFolder);
@@ -170,31 +178,18 @@ public class MyNotesView extends Div {
 		noteList.setRenderer(noteCardRenderer);
 		noteList.setMinWidth(MIN_WIDTH);
 
-		Button addFolder = new Button("Hinzufügen", e ->{
-			currentFolder = new Folder(LEER, false, null, null);
-			folderService.update(currentFolder);
-			currentNote = new Note(LEER, LEER, currentFolder);
-			noteService.update(currentNote);
-			folders = folderService.list();
-			notes = folders.stream().findFirst().get().getNotes();
-
-			folderList.setItems(folders);
+		Button addFolder = new Button("Neuer Ordner", e -> {
+			newFolderOrNote(true);
 		});
 
 		VerticalLayout listLayout = new VerticalLayout(addFolder, folderList);
 
-		Button addNote = new Button("Hinzufügen", e -> {
-			currentNote = new Note(LEER, LEER, currentFolder);
-			noteService.update(currentNote);
-			folders = folderService.list();
-			notes = folderService.get(currentFolder.getId()).get().getNotes();
+		Button addNote = new Button("Neue Notiz", e -> {
 
-			folderList.setItems(folders);
-			noteList.setItems(notes);
+			newFolderOrNote(false);
 		});
-
 		VerticalLayout listLayoutN = new VerticalLayout(addNote, noteList);
-
+		listLayoutN.setMaxWidth("350px");
 		notesSplit.setPadding(true);
 		notesSplit.add(listLayoutN);
 		notesSplit.add(getNormalLayout());
@@ -205,6 +200,83 @@ public class MyNotesView extends Div {
 		splitLayout.addThemeVariants(SplitLayoutVariant.LUMO_MINIMAL);
 
 		return splitLayout;
+	}
+
+	private void checkUser() {
+		if (!Objects.isNull(authenticatedUser)) {
+			if (authenticatedUser.get().isPresent()) {
+				user = authenticatedUser.get().get();
+			} else {
+				authenticatedUser.logout();
+				UI.getCurrent().navigate("login");
+			}
+		} else {
+			
+			UI.getCurrent().navigate("login");
+		}
+	}
+
+	private void newFolderOrNote(boolean sourceFromFolderButton) {
+		Dialog dialog;
+		dialog = new Dialog();
+		dialog.setHeaderTitle("Neue Firma");
+
+		VerticalLayout dialogLayout = createDialogLayout();
+
+		dialog.add(dialogLayout);
+
+		Button saveButton = new Button("speichern", b -> {
+			if (sourceFromFolderButton) {
+				emptyFolder();
+			} else {
+				newNote();
+			}
+
+			folderList.setItems(folders);
+
+			dialog.close();
+		});
+		saveButton.addClickShortcut(Key.ENTER);
+		Button cancelButton = new Button("Cancel", be -> dialog.close());
+
+		dialog.getFooter().add(saveButton);
+		dialog.getFooter().add(cancelButton);
+
+		add(dialog);
+
+		dialog.open();
+	}
+
+	private void newNote() {
+		currentNote = new Note(LEER, LEER, currentFolder);
+		noteService.update(currentNote);
+		folders = folderService.list();
+		notes = folderService.get(currentFolder.getId()).get().getNotes();
+
+		folderList.setItems(folders);
+		noteList.setItems(notes);
+	}
+
+	private VerticalLayout createDialogLayout() {
+
+		VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.add(popupField);
+		verticalLayout.setPadding(false);
+		verticalLayout.setSpacing(false);
+		verticalLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+		verticalLayout.getStyle().set("width", "18rem").set("max-width", "100%");
+
+		return verticalLayout;
+
+	}
+
+	private void emptyFolder() {
+		currentFolder = new Folder(LEER, false, null, user);
+		folderService.update(currentFolder);
+		currentNote = new Note(LEER, LEER, currentFolder);
+		noteService.update(currentNote);
+		folders = folderService.list();
+		notes = folders.stream().findFirst().get().getNotes();
 	}
 
 	private VerticalLayout getNormalLayout() {
