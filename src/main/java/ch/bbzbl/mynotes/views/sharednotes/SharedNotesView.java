3999b458f3d258f3a0bd17b9c4cc3f12b00cad3e
iventhepro <iven.kuder@bbzbl-it.ch>
2023-04-14 15:17:20 +0200

    sharedView billig impl

--- src/main/java/ch/bbzbl/mynotes/views/sharednotes/SharedNotesView.java
+++ src/main/java/ch/bbzbl/mynotes/views/sharednotes/SharedNotesView.java
@@ -1,8 +1,11 @@
 package ch.bbzbl.mynotes.views.sharednotes;
 
+import ch.bbzbl.mynotes.components.appnav.NotesViewLayout;
 import ch.bbzbl.mynotes.data.entity.Folder;
 import ch.bbzbl.mynotes.data.service.FolderService;
 import ch.bbzbl.mynotes.data.service.NoteService;
+import ch.bbzbl.mynotes.data.service.UserService;
+import ch.bbzbl.mynotes.security.AuthenticatedUser;
 import ch.bbzbl.mynotes.views.MainLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
@@ -17,29 +20,12 @@ import jakarta.annotation.security.PermitAll;
 @Route(value = "shared-notes", layout = MainLayout.class)
 @PermitAll
 public class SharedNotesView extends VerticalLayout {
-    private FolderService folderService = null;
-    private NoteService noteService = null;
 
-    public SharedNotesView(FolderService folderService, NoteService noteService) {
-        setSpacing(false);
-        this.folderService = folderService;
-        this.noteService = noteService;
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+	public SharedNotesView(FolderService folderService, NoteService noteService, UserService userService, AuthenticatedUser authenticatedUser) {
+		setHeightFull();
+		NotesViewLayout notesViewLayout = new NotesViewLayout(folderService, noteService, userService, authenticatedUser, true);
+		add(notesViewLayout);
 
-        for (Folder folder : folderService.getPublicFolders()) {
-            add(new Paragraph(folder.getTitel()));
-        }
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
-    }
+	}
 
 }
